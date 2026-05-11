6cb3707b8074e0e20073b7157a1a35aa6db512e1
Ricardo Fuster Arcos <165805818+ritchie13jr@users.noreply.github.com>
2026-05-11 11:11:17 +0200

    MenuTransition

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -1,8 +1,36 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        GameObject backButtonObj = GameObject.FindGameObjectWithTag("BackToMenu");
+
+        if (backButtonObj != null)
+        {
+            Button button = backButtonObj.GetComponent<Button>();
+
+            if (button != null)
+            {
+                button.onClick.RemoveAllListeners();
+                button.onClick.AddListener(LoadMenu);
+            }
+        }
+    }
+
+
     public void LoadSkillTree() 
     {
         SceneManager.LoadScene(NameLevels.SkillTree.ToString());
